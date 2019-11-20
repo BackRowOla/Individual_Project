79be3c87cba15ca73d4244f05ed13fc1375c455d
Babalakin Oyeyemi <oyeyemi.ola@gmail.com>
2019-11-20 01:01:49 +0000

    Update and delete working for recipes

--- Scripts/recipe.js
+++ Scripts/recipe.js
@@ -13,14 +13,13 @@ let recipeData;
 getRecipes();
 
 document.getElementById('updateBtn').addEventListener('click', (event) => updateRecipe());
-// document.getElementById('deleteBtn').addEventListener('click', (event) => deleteRecipe());
 
 function getRecipes() {
     fetch('http://localhost:8080/recipe/' + recipeId)
         .then(res => res.json())
         .then(json => getRecipeDetails(json))
         .catch(err => console.error(err));
-    console.log('show a thing');
+    // console.log('show a thing');
 }
 
 function getRecipeDetails(data) {
@@ -28,9 +27,9 @@ function getRecipeDetails(data) {
     data.map(recipe => {
         document.getElementById('card-header').appendChild(constructElement('h3', recipe.name));
         let content =
-            'Cooking Time: ' + recipe.cooking_length + 'mins' + ' | ' + recipe.meal_period + '\n'
-            + '\nIngredients:\n' + recipe.ingredients + '\n'
-            + '\nCooking Method:\n' + recipe.cooking_method;
+            'Cooking Time: ' + recipe.cooking_length + ' mins' + ' | ' + recipe.meal_period + '\n' +
+            '\nIngredients:\n' + recipe.ingredients + '\n' +
+            '\nCooking Method:\n' + recipe.cooking_method;
         document.getElementById('card-body').appendChild(constructElement('p', content));
     });
     return data;
@@ -42,8 +41,8 @@ function constructElement(element, content) {
     return myEl;
 }
 
-function updateRecipe(){
-    console.log(recipeData);
+function updateRecipe() {
+    // console.log(recipeData);
     recipeData.map(r => {
         mName.value = r.name;
         mCookingLength.value = r.cooking_length;
@@ -53,20 +52,39 @@ function updateRecipe(){
     });
 }
 
-function postData(){
 
+document.getElementById('btnUpdateRecipe').addEventListener('click', (event) => postData());
+document.getElementById('btnDeleteRecipe').addEventListener('click', (event) => removeData());
+
+function postData() {
+    recipeData.map(r => {
+        // console.log(r.id);
+        fetch('http://localhost:8080/recipe/update/' + r.id, {
+            method: 'PUT',
+            body: JSON.stringify({
+                "name": mName.value,
+                "cooking_length": mCookingLength.value,
+                "meal_period": mMealPeriod.value,
+                "ingredients": mIngredients.value,
+                "cooking_method": mCookingMethod.value
+            }),
+            headers: { "Content-Type": "application/json" }
+        });
+        alert('updated successfully');
+        window.location.href = './recipe.html?id=' + r.id;
+    });
 }
 
-function deleteRecipe(){
-
-}
-
-function removeData(id){
-    fetch('http://localhost:8080/recipe/delete', {
-        method: 'DELETE',
-        body: JSON.stringify({
-            "id": id
-        }),
-        headers: {"Content-Type": "application/json"}
+function removeData() {
+    recipeData.map(r => {
+        fetch('http://localhost:8080/recipe/delete', {
+            method: 'DELETE',
+            body: JSON.stringify({
+                "id": r.id
+            }),
+            headers: { "Content-Type": "application/json" }
+        });
+        alert('Recipe deleted');
+        window.location.href = './home.html';
     });
 }
--- routes/recipe-routes.js
+++ routes/recipe-routes.js
@@ -46,12 +46,28 @@ router.post('/add', async(req, res) => {
     res.status(201).send('Recipe added successfully');
 });
 
+router.put('/update/:id', async(req, res) => {
+    await models.Recipe.update({
+        name: req.body.name,
+        cooking_length: req.body.cooking_length,
+        meal_period: req.body.meal_period,
+        ingredients: req.body.ingredients,
+        cooking_method: req.body.cooking_method
+    }, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(201).send('Recipe updated');
+});
+
 router.delete('/delete', async(req, res) => {
     await models.Recipe.destroy({
         where: {
             id: req.body.id
         }
     });
-    res.send('Recipe deleted');
+    res.status(201).send('Recipe deleted');
 });
+
 module.exports = router;
